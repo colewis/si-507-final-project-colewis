f6bbb555f4a0f4f4e6ec9c9eb56bfae239dd1386
colby lewis <colewis@umich.edu>
2018-04-11 17:43:28 -0400

    pushing progress

--- fetch_dogs.py
+++ fetch_dogs.py
@@ -83,28 +83,48 @@ group_list.insert(76, 'Hound Group')
 group_list.insert(118, 'Hound Group')
 group_list.insert(155, 'Non-Sporting Group')
 group_list.insert(185, 'Hound Group')
-#print(len(breed_list)) 265
-print(len(group_list)) #264 - looking for 1 incorrect one
+group_list.insert(210, 'Working Group')
 
+### write code to go through each category by subcategory (new baseurl per category, such as
+### http://www.akc.org/dog-breeds/?coat_type%5B%5D=short, http://www.akc.org/dog-breeds/ page/2/ ?coat_type%5B0%5D=short)
+### and scrape those pages to make a list of what breeds fall in those categories
+### then make a db to match up foreign keys (check project 3 code)
 
 #categories_list = ['Group', 'Activity Level', 'Barking Level', 'Characteristics', 'Coat Type', 'Shedding', 'Size', 'Trainability']
 dog_dict = {}
-# for x in breed_list:
-#     dog_dict[x] = {}
-#     for y in range(len(group_list)):
-#         dog_dict[x]['Group'] = group_list[y]
-for y in range(len(group_list)):
+for y in range(len(breed_list)):
     dog_dict[(breed_list[y])] = {}
     dog_dict[(breed_list[y])]['Group'] = group_list[y]
 
-print(dog_dict)
+# my_dict = {'App 1': 'App id1', 'App 2': 'App id2', 'App 3': 'App id3'}
+# with open('test.csv', 'w') as f:
+#     fieldnames = ['Application Name', 'Application ID']
+#     writer = csv.DictWriter(f, fieldnames=fieldnames)
+#     writer.writeheader()
+#     data = [dict(zip(fieldnames, [k, v])) for k, v in my_dict.items()]
+#     writer.writerows(data)
+#
+# my_dict = {'App 1': 'App id1', 'App 2': 'App id2', 'App 3': 'App id3'}
+# with open('test.csv', 'w') as f:
+#     f.write('Application Name, Application ID\n')
+#     for key in my_dict.keys():
+#         f.write("%s,%s\n"%(key,my_dict[key]))
 
-# print(len(group_list)) 258
-# print(len(breed_list)) 264
+###figure out how to write dictionary to csv
 
+with open('master_list.csv', 'w') as f:
+    f.write('Breed, Group\n')
+    for key in dog_dict.keys():
+        f.write("%s,%s\n"%(key,dog_dict[key]['Group']))
+
+f.close()
+
+MASTERCSV = 'master_list.csv'
 
 # with open('breed_list.csv', 'w', newline='') as csv_file:
 #     get_dog = csv.writer(csv_file, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+#     headers = ['Breed', 'Group']
+#     get_dog.writeheader()
 #     for puppy in breed_list:
 #         get_dog.writerow([puppy])
 # csv_file.close()
@@ -120,185 +140,208 @@ print(dog_dict)
 #something along the lines of "if <whatever tag text> == dog_dict[key/breedname], then
 #dog_dict[k/b][0] = <whatever group>, dog_dict[k/b][1] = <whatever activity level>, etc"
 
-# def init_db(db_name): #creates/initializes the database
-#
-#     try:
-#         conn = sqlite3.connect(DBNAME)
-#         cur = conn.cursor()
-#     except Error as e:
-#         print(e)
-#
-#     statement = '''
-#         DROP TABLE IF EXISTS 'Breeds';
-#     '''
-#     cur.execute(statement)
-#     conn.commit()
-#     make_table = '''
-#         CREATE TABLE IF NOT EXISTS 'Breeds' (
-#         'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-#         'Breed Name' TEXT
-#         );
-#     '''
-#     cur.execute(make_table)
-#     conn.commit()
-#
-#     statement1 = '''
-#         DROP TABLE IF EXISTS 'Groups';
-#     '''
-#     cur.execute(statement1)
-#     conn.commit()
-#     make_table1 = '''
-#         CREATE TABLE IF NOT EXISTS 'Groups' (
-#             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-#             'Sporting Group' TEXT,
-#             'Working Group' TEXT,
-#             'Toy Group' TEXT,
-#             'Herding Group' TEXT,
-#             'Foundation Stock Service' TEXT,
-#             'Hound Group' TEXT,
-#             'Terrier Group' TEXT,
-#             'Non-Sporting Group' TEXT,
-#             'Miscellaneous Class' TEXT
-#             --FOREIGN KEY('BroadBeanOriginId') REFERENCES 'Countries'('Id'),
-#             --FOREIGN KEY('CompanyLocationId') REFERENCES 'Countries'('Id')
-#         );
-#     '''
-#     cur.execute(make_table1)
-#     conn.commit()
-#
-#
-#     statement2 = '''
-#         DROP TABLE IF EXISTS 'Activity_Level';
-#     '''
-#     cur.execute(statement2)
-#     conn.commit()
-#     make_table2 = '''
-#         CREATE TABLE IF NOT EXISTS 'Activity_Level' (
-#             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-#             'Couch Potato' TEXT,
-#             'Regular Exercise' TEXT,
-#             'Calm' TEXT,
-#             'Needs Lots Of Activity' TEXT,
-#             'Energetic' TEXT
-#         );
-#     '''
-#     cur.execute(make_table2)
-#     conn.commit()
-#
-#     statement3 = '''
-#         DROP TABLE IF EXISTS 'Barking_Level';
-#     '''
-#     cur.execute(statement3)
-#     conn.commit()
-#     make_table3 = '''
-#         CREATE TABLE IF NOT EXISTS 'Barking_Level' (
-#             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-#             'When Necessary' TEXT,
-#             'Medium' TEXT,
-#             'Likes To Be Vocal' TEXT,
-#             'Infrequent' TEXT,
-#             'Frequent' TEXT
-#         );
-#     '''
-#     cur.execute(make_table3)
-#     conn.commit()
-#
-#     statement4 = '''
-#         DROP TABLE IF EXISTS 'Characteristics';
-#     '''
-#     cur.execute(statement4)
-#     conn.commit()
-#     make_table4 = '''
-#         CREATE TABLE IF NOT EXISTS 'Characteristics' (
-#             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-#             'Smallest Dog Breeds' TEXT,
-#             'Largest Dog Breeds' TEXT,
-#             'Hypoallergenic Dogs' TEXT,
-#             'Best Guard Dogs' TEXT,
-#             'Best Dogs For Apartment Dwellers' TEXT,
-#             'Medium Dog Breeds' TEXT,
-#             'Smartest Dogs' TEXT,
-#             'Best Family Dogs' TEXT,
-#             'Best Dog Breeds For Children' TEXT,
-#             'Hairless Dog Breeds' TEXT
-#         );
-#     '''
-#     cur.execute(make_table4)
-#     conn.commit()
-#
-#     statement5 = '''
-#         DROP TABLE IF EXISTS 'Coat_Type';
-#     '''
-#     cur.execute(statement5)
-#     conn.commit()
-#     make_table5 = '''
-#         CREATE TABLE IF NOT EXISTS 'Coat_Type' (
-#             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-#             'Hairless' TEXT,
-#             'Medium' TEXT,
-#             'Smooth' TEXT,
-#             'Short' TEXT,
-#             'Long' TEXT,
-#             'Wire' TEXT
-#         );
-#     '''
-#     cur.execute(make_table5)
-#     conn.commit()
-#
-#     statement6 = '''
-#         DROP TABLE IF EXISTS 'Shedding';
-#     '''
-#     cur.execute(statement6)
-#     conn.commit()
-#     make_table6 = '''
-#         CREATE TABLE IF NOT EXISTS 'Shedding' (
-#             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-#             'Infrequent' TEXT,
-#             'Frequent' TEXT,
-#             'Regularly' TEXT,
-#             'Seasonal' TEXT,
-#             'Occasional' TEXT
-#         );
-#     '''
-#     cur.execute(make_table6)
-#     conn.commit()
-#
-#     statement7 = '''
-#         DROP TABLE IF EXISTS 'Size';
-#     '''
-#     cur.execute(statement7)
-#     conn.commit()
-#     make_table7 = '''
-#         CREATE TABLE IF NOT EXISTS 'Size' (
-#             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-#             'XSmall' TEXT,
-#             'Small' TEXT,
-#             'Medium' TEXT,
-#             'Large' TEXT,
-#             'XLarge' TEXT
-#         );
-#     '''
-#     cur.execute(make_table7)
-#     conn.commit()
-#
-#     statement8 = '''
-#         DROP TABLE IF EXISTS 'Trainability';
-#     '''
-#     cur.execute(statement8)
-#     conn.commit()
-#     make_table8 = '''
-#         CREATE TABLE IF NOT EXISTS 'Trainability' (
-#             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-#             'May Be Stubborn' TEXT,
-#             'Eager To Please' TEXT,
-#             'Easy Training' TEXT,
-#             'Agreeable' TEXT,
-#             'Independent' TEXT
-#         );
-#     '''
-#     cur.execute(make_table8)
-#     conn.commit()
-#
-#     conn.close()
-#
-# init_db(DBNAME)
+def init_db(db_name): #creates/initializes the database
+
+    try:
+        conn = sqlite3.connect(DBNAME)
+        cur = conn.cursor()
+    except Error as e:
+        print(e)
+
+    statement = '''
+        DROP TABLE IF EXISTS 'Breeds';
+    '''
+    cur.execute(statement)
+    conn.commit()
+    make_table = '''
+        CREATE TABLE IF NOT EXISTS 'Breeds' (
+        'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+        'Breed Name' TEXT,
+        'Group' TEXT
+        --add other columns
+        );
+    '''
+    cur.execute(make_table)
+    conn.commit()
+
+    statement1 = '''
+        DROP TABLE IF EXISTS 'Groups';
+    '''
+    cur.execute(statement1)
+    conn.commit()
+    make_table1 = '''
+        CREATE TABLE IF NOT EXISTS 'Groups' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Sporting Group' TEXT,
+            'Working Group' TEXT,
+            'Toy Group' TEXT,
+            'Herding Group' TEXT,
+            'Foundation Stock Service' TEXT,
+            'Hound Group' TEXT,
+            'Terrier Group' TEXT,
+            'Non-Sporting Group' TEXT,
+            'Miscellaneous Class' TEXT
+            --FOREIGN KEY('BroadBeanOriginId') REFERENCES 'Countries'('Id'),
+            --FOREIGN KEY('CompanyLocationId') REFERENCES 'Countries'('Id')
+        );
+    '''
+    cur.execute(make_table1)
+    conn.commit()
+
+
+    statement2 = '''
+        DROP TABLE IF EXISTS 'Activity_Level';
+    '''
+    cur.execute(statement2)
+    conn.commit()
+    make_table2 = '''
+        CREATE TABLE IF NOT EXISTS 'Activity_Level' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Couch Potato' TEXT,
+            'Regular Exercise' TEXT,
+            'Calm' TEXT,
+            'Needs Lots Of Activity' TEXT,
+            'Energetic' TEXT
+        );
+    '''
+    cur.execute(make_table2)
+    conn.commit()
+
+    statement3 = '''
+        DROP TABLE IF EXISTS 'Barking_Level';
+    '''
+    cur.execute(statement3)
+    conn.commit()
+    make_table3 = '''
+        CREATE TABLE IF NOT EXISTS 'Barking_Level' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'When Necessary' TEXT,
+            'Medium' TEXT,
+            'Likes To Be Vocal' TEXT,
+            'Infrequent' TEXT,
+            'Frequent' TEXT
+        );
+    '''
+    cur.execute(make_table3)
+    conn.commit()
+
+    statement4 = '''
+        DROP TABLE IF EXISTS 'Characteristics';
+    '''
+    cur.execute(statement4)
+    conn.commit()
+    make_table4 = '''
+        CREATE TABLE IF NOT EXISTS 'Characteristics' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Smallest Dog Breeds' TEXT,
+            'Largest Dog Breeds' TEXT,
+            'Hypoallergenic Dogs' TEXT,
+            'Best Guard Dogs' TEXT,
+            'Best Dogs For Apartment Dwellers' TEXT,
+            'Medium Dog Breeds' TEXT,
+            'Smartest Dogs' TEXT,
+            'Best Family Dogs' TEXT,
+            'Best Dog Breeds For Children' TEXT,
+            'Hairless Dog Breeds' TEXT
+        );
+    '''
+    cur.execute(make_table4)
+    conn.commit()
+
+    statement5 = '''
+        DROP TABLE IF EXISTS 'Coat_Type';
+    '''
+    cur.execute(statement5)
+    conn.commit()
+    make_table5 = '''
+        CREATE TABLE IF NOT EXISTS 'Coat_Type' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Hairless' TEXT,
+            'Medium' TEXT,
+            'Smooth' TEXT,
+            'Short' TEXT,
+            'Long' TEXT,
+            'Wire' TEXT
+        );
+    '''
+    cur.execute(make_table5)
+    conn.commit()
+
+    statement6 = '''
+        DROP TABLE IF EXISTS 'Shedding';
+    '''
+    cur.execute(statement6)
+    conn.commit()
+    make_table6 = '''
+        CREATE TABLE IF NOT EXISTS 'Shedding' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Infrequent' TEXT,
+            'Frequent' TEXT,
+            'Regularly' TEXT,
+            'Seasonal' TEXT,
+            'Occasional' TEXT
+        );
+    '''
+    cur.execute(make_table6)
+    conn.commit()
+
+    statement7 = '''
+        DROP TABLE IF EXISTS 'Size';
+    '''
+    cur.execute(statement7)
+    conn.commit()
+    make_table7 = '''
+        CREATE TABLE IF NOT EXISTS 'Size' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'XSmall' TEXT,
+            'Small' TEXT,
+            'Medium' TEXT,
+            'Large' TEXT,
+            'XLarge' TEXT
+        );
+    '''
+    cur.execute(make_table7)
+    conn.commit()
+
+    statement8 = '''
+        DROP TABLE IF EXISTS 'Trainability';
+    '''
+    cur.execute(statement8)
+    conn.commit()
+    make_table8 = '''
+        CREATE TABLE IF NOT EXISTS 'Trainability' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'May Be Stubborn' TEXT,
+            'Eager To Please' TEXT,
+            'Easy Training' TEXT,
+            'Agreeable' TEXT,
+            'Independent' TEXT
+        );
+    '''
+    cur.execute(make_table8)
+    conn.commit()
+
+    conn.close()
+
+init_db(DBNAME)
+
+def insert_dog_data():
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+
+    with open(MASTERCSV) as master_csv:
+        csvReader = csv.reader(master_csv)
+        for row in list(csvReader)[1:]:
+            insertion = (row[0], row[1])
+            statement = 'INSERT INTO "Breeds"'
+            statement += 'VALUES (NULL, ?, ?)'
+            cur.execute(statement, insertion)
+            conn.commit()
+
+    conn.close()
+
+insert_dog_data()
+
+### what I realistically probably want to do is create a master list/db with all of the information by breed
+### and then create dbs that draw from that list, so I could make a "Medium" dogs db, which would include just
+### dogs with "Medium" as their size and then all of their respective characteristics from the master list
