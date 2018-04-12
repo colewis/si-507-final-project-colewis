44804f4f201f6f509fdaa215ef80f79e57f9fcaa
colby lewis <colewis@umich.edu>
2018-04-12 16:00:57 -0400

    done scraping data!

--- fetch_dogs.py
+++ fetch_dogs.py
@@ -88,18 +88,26 @@ group_list.insert(185, 'Hound Group')
 group_list.insert(210, 'Working Group')
 
 ###URL PIECES###
-
-def crawl_urls(x,y,category,string,list):
-    for i in range(x,y):
-        crawlurl = page + str(i) + category + gibberish + string
-        list.append(baseurl + crawlurl)
-    return list
-
 page = 'page/'
 gibberish = '%5B0%5D='
+
+def crawl_urls(x,y,category,string,list):
+    if x != y:
+        for i in range(x,y):
+            crawlurl = page + str(i) + category + gibberish + string
+            list.append(baseurl + crawlurl)
+        return list
+    else:
+        crawlurl = category[1:] + gibberish + string
+        list.append(baseurl + crawlurl)
+        return list
+
 activity_level = '/?activity_level'
 barking_level = '/?barking_level'
-characteristic = '/?characteristic'
+coat_type = '/?coat_type'
+shedding = '/?shedding'
+size = '/?size'
+trainability = '/?trainability'
 
 ###ACTIVITY LEVEL###
 
@@ -149,49 +157,114 @@ scrape_breed_names(talkative, vocal)
 scrape_breed_names(in_frequent, infrequent)
 scrape_breed_names(out_frequent, frequent)
 
-###CHARACTERISTICS###
-
-smallest_breeds = []
-largest_breeds = []
-hypoallergenic_breeds = []
-best_guard_breeds = []
-best_for_apt = []
-medium_dog_breeds = []
-smartest_dogs = []
-best_fam_dogs = []
-best_for_kids = []
-best_hairless = []
-
-crawl_urls(1,6,characteristic,'smallest-dog-breeds',smallest_breeds)
-crawl_urls(1,5,characteristic,'largest-dog-breeds',largest_breeds)
-crawl_urls(1,3,characteristic,'hypoallergenic-dogs',hypoallergenic_breeds)
-crawl_urls(1,3,characteristic,'best-guard-dogs',best_guard_breeds)
-
-small_breeds = []
-large_breeds = []
-hypo_breeds = []
-guard_breeds = []
-apt_breeds = []
-med_breeds = []
-smart_breeds = []
-fam_breeds = []
-kids_breeds = []
-hairless_breeds = []
-
-scrape_breed_names(smallest_breeds, small_breeds)
-scrape_breed_names(largest_breeds, large_breeds)
-scrape_breed_names(hypoallergenic_breeds, hypo_breeds)
-scrape_breed_names(best_guard_breeds, guard_breeds)
-
-
-
 ###COAT TYPE###
+
+hairless = []
+medium_hair = []
+smooth = []
+short_hair = []
+long_hair = []
+wire = []
+
+crawl_urls(1,1,coat_type,'hairless',hairless)
+crawl_urls(1,10,coat_type,'medium',medium_hair)
+crawl_urls(1,1,coat_type,'smooth',smooth)
+crawl_urls(1,10,coat_type,'short',short_hair)
+crawl_urls(1,5,coat_type,'long',long_hair)
+crawl_urls(1,1,coat_type,'wire',wire)
+
+no_hair = []
+med_hair = []
+smooth_hair = []
+short_hair_dogs = []
+long_hair_dogs = []
+wire_hair = []
+
+scrape_breed_names(hairless, no_hair)
+scrape_breed_names(medium_hair, med_hair)
+scrape_breed_names(smooth, smooth_hair)
+scrape_breed_names(short_hair, short_hair_dogs)
+scrape_breed_names(long_hair, long_hair_dogs)
+scrape_breed_names(wire, wire_hair)
 
 ###SHEDDING###
 
+infrq_shed = []
+frq_shed = []
+reg_shed = []
+seas_shed = []
+occ_shed = []
+
+crawl_urls(1,4,shedding,'infrequent',infrq_shed)
+crawl_urls(1,1,shedding,'frequent',frq_shed)
+crawl_urls(1,4,shedding,'regularly',reg_shed)
+crawl_urls(1,11,shedding,'seasonal',seas_shed)
+crawl_urls(1,6,shedding,'occasional',occ_shed)
+
+infrq_shedder = []
+frq_shedder = []
+reg_shedder = []
+seas_shedder = []
+occ_shedder = []
+
+scrape_breed_names(infrq_shed, infrq_shedder)
+scrape_breed_names(frq_shed, frq_shedder)
+scrape_breed_names(reg_shed, reg_shedder)
+scrape_breed_names(seas_shed, seas_shedder)
+scrape_breed_names(occ_shed, occ_shedder)
+
 ###SIZE###
 
+sz_xsmall = []
+sz_small = []
+sz_medium = []
+sz_large = []
+sz_xlarge = []
+
+crawl_urls(1,3,size,'xsmall',sz_xsmall)
+crawl_urls(1,6,size,'small',sz_small)
+crawl_urls(1,10,size,'medium',sz_medium)
+crawl_urls(1,6,size,'large',sz_large)
+crawl_urls(1,3,size,'xlarge',sz_xlarge)
+
+xs_dogs = []
+small_dogs = []
+med_dogs = []
+large_dogs = []
+xl_dogs = []
+
+scrape_breed_names(sz_xsmall, xs_dogs)
+scrape_breed_names(sz_small, small_dogs)
+scrape_breed_names(sz_medium, med_dogs)
+scrape_breed_names(sz_large, large_dogs)
+scrape_breed_names(sz_xlarge, xl_dogs)
+
 ###TRAINABILITY###
+
+may_be_stubborn = []
+eager_to_please = []
+easy_training = []
+agreeable = []
+independent = []
+
+crawl_urls(1,3,trainability,'may-be-stubborn',may_be_stubborn)
+crawl_urls(1,6,trainability,'eager-to-please',eager_to_please)
+crawl_urls(1,5,trainability,'easy-training',easy_training)
+crawl_urls(1,8,trainability,'agreeable',agreeable)
+crawl_urls(1,7,trainability,'independent',independent)
+
+stubborn = []
+eager = []
+easy = []
+agree = []
+indp = []
+
+scrape_breed_names(may_be_stubborn, stubborn)
+scrape_breed_names(eager_to_please, eager)
+scrape_breed_names(easy_training, easy)
+scrape_breed_names(agreeable, agree)
+scrape_breed_names(independent, indp)
+
 
 ### write code to go through each category by subcategory (new baseurl per category, such as
 ### http://www.akc.org/dog-breeds/?coat_type%5B%5D=short, http://www.akc.org/dog-breeds/ page/2/ ?coat_type%5B0%5D=short)
@@ -200,7 +273,7 @@ scrape_breed_names(best_guard_breeds, guard_breeds)
 
 
 
-#'Characteristics', 'Coat Type', 'Shedding', 'Size', 'Trainability'
+#'Coat Type', 'Shedding', 'Size', 'Trainability'
 dog_dict = {}
 for y in range(len(breed_list)):
     dog_dict[(breed_list[y])] = {}
@@ -227,14 +300,65 @@ for y in range(len(breed_list)):
         dog_dict[(breed_list[y])]['Barking Level'] = 'Frequent'
     else:
         dog_dict[(breed_list[y])]['Barking Level'] = 'Not Specified'
+    if breed_list[y] in no_hair:
+        dog_dict[(breed_list[y])]['Coat Type'] = 'Hairless'
+    elif breed_list[y] in med_hair:
+        dog_dict[(breed_list[y])]['Coat Type'] = 'Medium'
+    elif breed_list[y] in smooth_hair:
+        dog_dict[(breed_list[y])]['Coat Type'] = 'Smooth'
+    elif breed_list[y] in short_hair_dogs:
+        dog_dict[(breed_list[y])]['Coat Type'] = 'Short'
+    elif breed_list[y] in long_hair_dogs:
+        dog_dict[(breed_list[y])]['Coat Type'] = 'Long'
+    elif breed_list[y] in wire_hair:
+        dog_dict[(breed_list[y])]['Coat Type'] = 'Wire'
+    else:
+        dog_dict[(breed_list[y])]['Coat Type'] = 'Not Specified'
+    if breed_list[y] in infrq_shedder:
+        dog_dict[(breed_list[y])]['Shedding'] = 'Infrequent'
+    elif breed_list[y] in frq_shedder:
+        dog_dict[(breed_list[y])]['Shedding'] = 'Frequent'
+    elif breed_list[y] in reg_shedder:
+        dog_dict[(breed_list[y])]['Shedding'] = 'Regularly'
+    elif breed_list[y] in seas_shedder:
+        dog_dict[(breed_list[y])]['Shedding'] = 'Seasonal'
+    elif breed_list[y] in occ_shedder:
+        dog_dict[(breed_list[y])]['Shedding'] = 'Occasional'
+    else:
+        dog_dict[(breed_list[y])]['Shedding'] = 'Not Specified'
+    if breed_list[y] in xs_dogs:
+        dog_dict[(breed_list[y])]['Size'] = 'XSmall'
+    elif breed_list[y] in small_dogs:
+        dog_dict[(breed_list[y])]['Size'] = 'Small'
+    elif breed_list[y] in med_dogs:
+        dog_dict[(breed_list[y])]['Size'] = 'Medium'
+    elif breed_list[y] in large_dogs:
+        dog_dict[(breed_list[y])]['Size'] = 'Large'
+    elif breed_list[y] in xl_dogs:
+        dog_dict[(breed_list[y])]['Size'] = 'XLarge'
+    else:
+        dog_dict[(breed_list[y])]['Size'] = 'Not Specified'
+    if breed_list[y] in stubborn:
+        dog_dict[(breed_list[y])]['Trainability'] = 'May Be Stubborn'
+    elif breed_list[y] in eager:
+        dog_dict[(breed_list[y])]['Trainability'] = 'Eager To Please'
+    elif breed_list[y] in easy:
+        dog_dict[(breed_list[y])]['Trainability'] = 'Easy Training'
+    elif breed_list[y] in agree:
+        dog_dict[(breed_list[y])]['Trainability'] = 'Agreeable'
+    elif breed_list[y] in indp:
+        dog_dict[(breed_list[y])]['Trainability'] = 'Independent'
+    else:
+        dog_dict[(breed_list[y])]['Trainability'] = 'Not Specified'
+
 
 
 ###CSV###
 
 with open('master_list.csv', 'w') as f:
-    f.write('Breed, Group, Activity_Level, Barking_Level\n')
+    f.write('Breed, Group, Activity_Level, Barking_Level, Coat_Type, Shedding, Size, Trainability\n') #, Shedding
     for key in dog_dict.keys():
-        f.write("%s,%s,%s,%s\n"%(key, dog_dict[key]['Group'], dog_dict[key]['Activity Level'], dog_dict[key]['Barking Level']))
+        f.write("%s,%s,%s,%s,%s,%s,%s,%s\n"%(key, dog_dict[key]['Group'], dog_dict[key]['Activity Level'], dog_dict[key]['Barking Level'], dog_dict[key]['Coat Type'], dog_dict[key]['Shedding'], dog_dict[key]['Size'], dog_dict[key]['Trainability']))
 
 f.close()
 
@@ -263,7 +387,6 @@ def init_db(db_name): #creates/initializes the database
         'Group' TEXT,
         'Activity Level' TEXT,
         'Barking Level' TEXT,
-        'Characteristics' TEXT,
         'Coat Type' TEXT,
         'Shedding' TEXT,
         'Size' TEXT,
@@ -276,21 +399,19 @@ def init_db(db_name): #creates/initializes the database
     conn.commit()
 
     statement1 = '''
-        DROP TABLE IF EXISTS 'Groups';
+        DROP TABLE IF EXISTS 'XS_Dogs';
     '''
     cur.execute(statement1)
     conn.commit()
     make_table1 = '''
-        CREATE TABLE IF NOT EXISTS 'Groups' (
-            'Sporting Group' TEXT,
-            'Working Group' TEXT,
-            'Toy Group' TEXT,
-            'Herding Group' TEXT,
-            'Foundation Stock Service' TEXT,
-            'Hound Group' TEXT,
-            'Terrier Group' TEXT,
-            'Non-Sporting Group' TEXT,
-            'Miscellaneous Class' TEXT
+        CREATE TABLE IF NOT EXISTS 'XS_Dogs' (
+        'Breed Name' TEXT,
+        'Group' TEXT,
+        'Activity Level' TEXT,
+        'Barking Level' TEXT,
+        'Coat Type' TEXT,
+        'Shedding' TEXT,
+        'Trainability' TEXT
         );
     '''
     cur.execute(make_table1)
@@ -298,129 +419,114 @@ def init_db(db_name): #creates/initializes the database
 
 
     statement2 = '''
-        DROP TABLE IF EXISTS 'Activity_Level';
+        DROP TABLE IF EXISTS 'Small_Dogs';
     '''
     cur.execute(statement2)
     conn.commit()
     make_table2 = '''
-        CREATE TABLE IF NOT EXISTS 'Activity_Level' (
-            'Couch Potato' TEXT,
-            'Regular Exercise' TEXT,
-            'Calm' TEXT,
-            'Needs Lots Of Activity' TEXT,
-            'Energetic' TEXT
+        CREATE TABLE IF NOT EXISTS 'Small_Dogs' (
+        'Breed Name' TEXT,
+        'Group' TEXT,
+        'Activity Level' TEXT,
+        'Barking Level' TEXT,
+        'Coat Type' TEXT,
+        'Shedding' TEXT,
+        'Trainability' TEXT
         );
     '''
     cur.execute(make_table2)
     conn.commit()
 
     statement3 = '''
-        DROP TABLE IF EXISTS 'Barking_Level';
+        DROP TABLE IF EXISTS 'Medium_Dogs';
     '''
     cur.execute(statement3)
     conn.commit()
     make_table3 = '''
-        CREATE TABLE IF NOT EXISTS 'Barking_Level' (
-            'When Necessary' TEXT,
-            'Medium' TEXT,
-            'Likes To Be Vocal' TEXT,
-            'Infrequent' TEXT,
-            'Frequent' TEXT
+        CREATE TABLE IF NOT EXISTS 'Medium_Dogs' (
+        'Breed Name' TEXT,
+        'Group' TEXT,
+        'Activity Level' TEXT,
+        'Barking Level' TEXT,
+        'Coat Type' TEXT,
+        'Shedding' TEXT,
+        'Trainability' TEXT
         );
     '''
     cur.execute(make_table3)
     conn.commit()
 
     statement4 = '''
-        DROP TABLE IF EXISTS 'Characteristics';
+        DROP TABLE IF EXISTS 'Large_Dogs';
     '''
     cur.execute(statement4)
     conn.commit()
     make_table4 = '''
-        CREATE TABLE IF NOT EXISTS 'Characteristics' (
-            'Smallest Dog Breeds' TEXT,
-            'Largest Dog Breeds' TEXT,
-            'Hypoallergenic Dogs' TEXT,
-            'Best Guard Dogs' TEXT,
-            'Best Dogs For Apartment Dwellers' TEXT,
-            'Medium Dog Breeds' TEXT,
-            'Smartest Dogs' TEXT,
-            'Best Family Dogs' TEXT,
-            'Best Dog Breeds For Children' TEXT,
-            'Hairless Dog Breeds' TEXT
+        CREATE TABLE IF NOT EXISTS 'Large_Dogs' (
+        'Breed Name' TEXT,
+        'Group' TEXT,
+        'Activity Level' TEXT,
+        'Barking Level' TEXT,
+        'Coat Type' TEXT,
+        'Shedding' TEXT,
+        'Trainability' TEXT
         );
     '''
     cur.execute(make_table4)
     conn.commit()
 
     statement5 = '''
-        DROP TABLE IF EXISTS 'Coat_Type';
+        DROP TABLE IF EXISTS 'XL_Dogs';
     '''
     cur.execute(statement5)
     conn.commit()
     make_table5 = '''
-        CREATE TABLE IF NOT EXISTS 'Coat_Type' (
-            'Hairless' TEXT,
-            'Medium' TEXT,
-            'Smooth' TEXT,
-            'Short' TEXT,
-            'Long' TEXT,
-            'Wire' TEXT
+        CREATE TABLE IF NOT EXISTS 'XL_Dogs' (
+        'Breed Name' TEXT,
+        'Group' TEXT,
+        'Activity Level' TEXT,
+        'Barking Level' TEXT,
+        'Coat Type' TEXT,
+        'Shedding' TEXT,
+        'Trainability' TEXT
         );
     '''
     cur.execute(make_table5)
     conn.commit()
 
-    statement6 = '''
-        DROP TABLE IF EXISTS 'Shedding';
-    '''
-    cur.execute(statement6)
-    conn.commit()
-    make_table6 = '''
-        CREATE TABLE IF NOT EXISTS 'Shedding' (
-            'Infrequent' TEXT,
-            'Frequent' TEXT,
-            'Regularly' TEXT,
-            'Seasonal' TEXT,
-            'Occasional' TEXT
-        );
-    '''
-    cur.execute(make_table6)
-    conn.commit()
-
-    statement7 = '''
-        DROP TABLE IF EXISTS 'Size';
-    '''
-    cur.execute(statement7)
-    conn.commit()
-    make_table7 = '''
-        CREATE TABLE IF NOT EXISTS 'Size' (
-            'XSmall' TEXT,
-            'Small' TEXT,
-            'Medium' TEXT,
-            'Large' TEXT,
-            'XLarge' TEXT
-        );
-    '''
-    cur.execute(make_table7)
-    conn.commit()
-
-    statement8 = '''
-        DROP TABLE IF EXISTS 'Trainability';
-    '''
-    cur.execute(statement8)
-    conn.commit()
-    make_table8 = '''
-        CREATE TABLE IF NOT EXISTS 'Trainability' (
-            'May Be Stubborn' TEXT,
-            'Eager To Please' TEXT,
-            'Easy Training' TEXT,
-            'Agreeable' TEXT,
-            'Independent' TEXT
-        );
-    '''
-    cur.execute(make_table8)
-    conn.commit()
+    # statement6 = '''
+    #     DROP TABLE IF EXISTS 'Size';
+    # '''
+    # cur.execute(statement6)
+    # conn.commit()
+    # make_table6 = '''
+    #     CREATE TABLE IF NOT EXISTS 'Size' (
+    #         'XSmall' TEXT,
+    #         'Small' TEXT,
+    #         'Medium' TEXT,
+    #         'Large' TEXT,
+    #         'XLarge' TEXT
+    #     );
+    # '''
+    # cur.execute(make_table6)
+    # conn.commit()
+    #
+    # statement7 = '''
+    #     DROP TABLE IF EXISTS 'Trainability';
+    # '''
+    # cur.execute(statement7)
+    # conn.commit()
+    # make_table7 = '''
+    #     CREATE TABLE IF NOT EXISTS 'Trainability' (
+    #         'May Be Stubborn' TEXT,
+    #         'Eager To Please' TEXT,
+    #         'Easy Training' TEXT,
+    #         'Agreeable' TEXT,
+    #         'Independent' TEXT
+    #     );
+    # '''
+    # cur.execute(make_table7)
+    # conn.commit()
 
     conn.close()
 
@@ -433,9 +539,9 @@ def insert_dog_data():
     with open(MASTERCSV) as master_csv:
         csvReader = csv.reader(master_csv)
         for row in list(csvReader)[1:]:
-            insertion = (row[0], row[1], row[2], row[3])
+            insertion = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]) #, row[5]
             statement = 'INSERT INTO "Breeds"'
-            statement += 'VALUES (NULL, ?, ?, ?, ?, NULL, NULL, NULL, NULL, NULL)'
+            statement += 'VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)'
             cur.execute(statement, insertion)
             conn.commit()
 
